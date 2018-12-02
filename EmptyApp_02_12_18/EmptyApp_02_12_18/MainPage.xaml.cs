dfd30d054760623fe88714feb907b559cd225fb7
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2018-12-02 20:15:08 +0300

    Add Empty App with linq2twitter 5.0.0

---
+++ EmptyApp_02_12_18/EmptyApp_02_12_18/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EmptyApp_02_12_18
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
